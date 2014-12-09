f36cbf2c6640fd886a9995f0e906963e0ca8276b
GLB-RAS <ramiro.sanchez@globant.com>
2014-12-09 19:03:46 -0300

    Google SignIn POC

--- api/routes/Base.php
+++ api/routes/Base.php
@@ -1,15 +1,31 @@
 <?php
-class Routes
+namespace Routes;
+
+class Base
 {
+    /**
+     * @var \Phalcon\Http\Request
+     */
+    public $request;
     /**
      * @var \Phalcon\Http\Response
      */
     public $response;
+    /**
+     * @var \Phalcon\Session\Adapter\Files
+     */
+    public $session;
 
-    public function __construct()
+    public function __construct(\Phalcon\DI\FactoryDefault $di)
     {
-        $this->response = new \Phalcon\Http\Response();
+        $this->request = $di->getShared('request');
+        $this->response = $di->getShared('response');
+        $this->session = $di->getShared('session');
+
+        $this->_init();
     }
+
+    protected function _init() {}
 
     public function pull($id = null)
     {
